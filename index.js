71d6464da0a0a73156610c1989d08d1cee843ad8
Naif Otibi <nn.n@outlook.cl>
2024-09-29 08:19:56 +0300

    javascript changes

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 
 const tabButtons = document.querySelectorAll("button[role='tab']");
 const tabPanels = document.querySelectorAll("div[role='tabpanel']");
+
+
+tabButtons.forEach((tb , index) => {
+    tb.addEventListener("click" , () => {
+        tabButtons.forEach((tb , index) => {
+            if(tb.getAttribute("aria-selected" , "true")){
+                tb.setAttribute("aria-selected" , "false");
+                tb.setAttribute("data-active" , "false");
+                tabPanels[index].setAttribute("data-active" , 'false')
+            }
+        });
+        tb.setAttribute("aria-selected" , "true");
+        tb.setAttribute("data-active" , "true");
+        tabPanels[index].setAttribute("data-active" , 'true')
+    });
+});
